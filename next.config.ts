010dfcf1ca722047f673367fa235a45e7a5c2153
Israel Bastarrica <israelbastarrica@marketarg.com>
2026-07-12 19:36:06 -0300

    Redirect www.najul.com.ar -> najul.com.ar (redirect por host en next.config)

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,16 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [{ type: "host", value: "www.najul.com.ar" }],
+        destination: "https://najul.com.ar/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
